7931915dd1fe66fffef06148b641a4d35b40d69e
thebooleanguy <tharindus0601@gmail.com>
2025-01-08 21:10:36 +0530

    Set up GreetingActivity

---
+++ app/src/main/java/lk/petpet/app/activities/GreetingActivity.kt
@@ -0,0 +1,23 @@
+package lk.petpet.app.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.petadoption.databinding.ActivityGreetingBinding
+
+class GreetingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGreetingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGreetingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
