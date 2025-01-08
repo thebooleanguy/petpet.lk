ad202e63f6b4c6f6de9d22d4ca9ac51c02a1091a
thebooleanguy <tharindus0601@gmail.com>
2025-01-08 21:11:11 +0530

    Set up utility class for image handling

---
+++ app/src/main/java/lk/petpet/app/models/ImageUtils.kt
@@ -0,0 +1,25 @@
+package lk.petpet.app.utils
+
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+object ImageUtils {
+    fun createImageFile(context: Context): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("JPEG_${timeStamp}_", ".jpg", storageDir)
+    }
+
+    fun getUriForFile(context: Context, file: File): Uri {
+        return FileProvider.getUriForFile(
+            context,
+            "${context.packageName}.fileprovider",
+            file
+        )
+    }
+}
